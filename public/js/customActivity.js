10cb599aaf0bead9a8749cbd196c692e26caa339
Kritika199 <kritikasingh38385@gmail.com>
2025-02-03 18:37:31 +0530

    hello test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -957,9 +957,9 @@ function validateStep3() {
 $(document).ready(function () {
   $('#expDelivery').change(function () {
       if ($(this).is(':checked')) {
-          $('#mailingClass3').closest('.input-box').addClass('hidden-options'); // Hide the entire field
+          $('#mailingClass3').closest('.input-box').addClass('hidden'); // Hide the entire field
       } else {
-          $('#mailingClass3').closest('.input-box').removeClass('hidden-options'); // Show the entire field
+          $('#mailingClass3').closest('.input-box').removeClass('visible'); // Show the entire field
       }
   });
 
